178c428d825a35434d7bb1b588913e41af0be24c
UO270185 <UO270185@uniovi.es>
2021-02-17 14:04:51 +0100

    SDI-2021-411-session2-Ejercicio-complementario3

--- sdi2021-411-spring/src/main/java/com/uniovi/controllers/ProfessorController.java
+++ sdi2021-411-spring/src/main/java/com/uniovi/controllers/ProfessorController.java
@@ -1,40 +1,48 @@
 package com.uniovi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.uniovi.entities.Professor;
 import com.uniovi.services.ProfessorService;
 
-@RestController
+@Controller
 public class ProfessorController {
 
 	@Autowired 
 	private ProfessorService professorService;
 
 	@RequestMapping("professor/list")
-	public String getList() {
-		return professorService.getProfessors().toString();
+	public String getList(Model model) {
+		model.addAttribute("professorList", professorService.getProfessors());
+		return "professor/list";
 	}
 
 	@RequestMapping(value = "/professor/add", method = RequestMethod.POST)
 	public String setProfessor(@ModelAttribute Professor professor) {
 		professorService.addProfessor(professor);
-		return "Added professor "+professor.getName() + " "+professor.getSurname()+", with dni "+professor.getDni()+" and occupation "+professor.getOccupation();
+		return "redirect:/professor/list";
 	}
 
 	@RequestMapping("/professor/details/{id}")
-	public String getDetail(@PathVariable Long id) {
-		return professorService.getProfessor(id).toString();
+	public String getDetail(Model model, @PathVariable Long id) {
+		model.addAttribute("professor", professorService.getProfessor(id));
+		return "professor/details/"+id;
 	}
 
 	@RequestMapping("/professor/delete/{id}")
 	public String deleteProfessor(@PathVariable Long id) {
 		professorService.deleteProfessor(id);
-		return "Deleting professor"; 
+		return "redirect:/professor/list"; 
+	}
+	
+	@RequestMapping(value="/professor/add")
+	public String getMark() {
+		return "professor/add";
 	}
 }
